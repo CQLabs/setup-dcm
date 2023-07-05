30ca0a5df511cc95052fabc772148e10b67927b5
Dmitry Zhifarsky <dimannich2380@gmail.com>
2023-07-05 18:47:00 +0400

    feat: implement action

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,97 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
+import * as tc from '@actions/tool-cache';
+import os from 'os';
+
+type Platform = 'windows' | 'macos' | 'linux';
+type Architecture = 'arm' | 'x64';
+
+const toolName = 'dcm';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token = core.getInput('github_token', { required: true });
 
-    core.debug(new Date().toTimeString());
-    core.debug(new Date().toTimeString());
+    const version = await getVersion(token);
+    const platform = getPlatform();
+    const architecture = getArchitecture();
 
-    core.setOutput('time', new Date().toTimeString());
+    core.info(`Installing DCM ${version} for ${platform}-${architecture}.`);
+
+    const url = getDownloadLink(version, platform, architecture);
+    const path = await downloadExe(url, version, platform, architecture);
+    core.addPath(path);
+
+    core.setOutput(`${toolName}-version`, version);
+
+    exec.exec(toolName, ['--version']);
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
+}
+
+async function getVersion(token: string): Promise<string> {
+  const version = core.getInput('version');
+
+  if (version === 'latest') {
+    const octokit = github.getOctokit(token);
+
+    const latestRelease = await octokit.rest.repos.getLatestRelease({
+      repo: 'homebrew-dcm',
+      owner: 'CQLabs',
+    });
+
+    return latestRelease.data.tag_name;
+  }
+
+  return version;
+}
+
+function getPlatform(): Platform {
+  const platform = os.platform();
+
+  if (platform === 'win32') return 'windows';
+  if (platform === 'darwin') return 'macos';
+
+  return 'linux';
+}
+
+function getArchitecture(): Architecture {
+  if (['arm', 'arm64'].includes(os.arch())) return 'arm';
+
+  return 'x64';
+}
+
+function getDownloadLink(version: string, platform: Platform, architecture: Architecture): string {
+  if (platform === 'windows') {
+    return `https://github.com/CQLabs/homebrew-dcm/releases/download/${version}/dcm-${platform}-release.zip`;
+  }
+
+  return `https://github.com/CQLabs/homebrew-dcm/releases/download/${version}/dcm-${platform}-${architecture}-release.zip`;
+}
+
+async function downloadExe(
+  url: string,
+  version: string,
+  platform: Platform,
+  architecture: Architecture,
+): Promise<string> {
+  const fromCache = tc.find(toolName, version, architecture);
+  if (fromCache !== '') {
+    core.info(`Using cached version from ${fromCache}.`);
+
+    return fromCache;
+  }
+
+  core.info(`Downloading from ${url}...`);
+
+  const archive = await tc.downloadTool(url);
+  const extracted = await tc.extractZip(archive);
+  const exeName = `${toolName}${platform === 'windows' ? '.exe' : ''}`;
+  const path = await tc.cacheFile(extracted, exeName, toolName, version, architecture);
+
+  return path;
 }
 
 run();
